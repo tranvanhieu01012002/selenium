be69f3186deb0e6100a0442bab3f71ff00f1efa8
tranvanhieu2002 <hieu.tran23@student.passerellesnumeriques.org>
2022-10-04 11:52:38 +0700

    push code

--- test.js
+++ test.js
@@ -1,13 +1,22 @@
 const {Builder, Browser, By, Key, until} = require('selenium-webdriver');
-
+const account = 'tranvanhieu01';
+const password = 'tranvanhieu2002';
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
 (async function example() {
     let driver = await new Builder().forBrowser(Browser.CHROME).build();
     try {
-      await driver.get('https://www.google.com/ncr');
-      await driver.findElement(By.name('q')).sendKeys('webdriver', Key.RETURN);
+      await driver.get('https://www.neobux.com/m/l/');
     
-      await driver.wait(until.titleIs('webdriver - Google Search'), 10000);
+      await driver.findElement(By.id('Kf1')).sendKeys(account, Key.RETURN);
+      await sleep(500)
+      await driver.findElement(By.id('Kf2')).sendKeys(password, Key.RETURN);
+      await sleep(5000)
+      await driver.findElement(By.className('button medium green')).click();
+      
     } finally {
-      await driver.quit();
+      console.log("success access");
+      // await driver.quit();
     }
   })();
